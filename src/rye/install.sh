92ec2ce572873014657ad0fe8bab1a32987774d0
David Birks <david@birks.dev>
2024-05-30 23:20:39 -0400

    Enable bash completion for rye

--- src/rye/install.sh
+++ src/rye/install.sh
@@ -7,3 +7,9 @@ echo "Activating feature 'rye'"
 
 # https://rye.astral.sh/guide/installation/#customized-installation
 curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
+
+if [ "$BASHCOMPLETION" == "true" ]; then
+  echo "Activating bash completion for 'rye'"
+  # https://rye.astral.sh/guide/installation/#shell-completion
+  rye self completion > /etc/bash_completion.d/rye
+fi
